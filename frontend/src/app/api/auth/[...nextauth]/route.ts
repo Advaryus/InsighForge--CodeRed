4fdc77fd53f71f5096d0d67a153348a82c7a2474
Prafull Anand <anandprafull007@gmail.com>
2025-01-09 16:55:40 +0530

    added custon credentials

--- frontend/src/app/api/auth/[...nextauth]/route.ts
+++ frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,7 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials"
+
 
 console.log('GOOGLE_CLIENT_ID:', process.env.AUTH_GOOGLE_ID);
 console.log('GOOGLE_CLIENT_SECRET:', process.env.AUTH_GOOGLE_SECRET);
@@ -11,7 +13,36 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.AUTH_GOOGLE_SECRET!,
       allowDangerousEmailAccountLinking: true
     }),
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        username: { label: "Username", type: "text" },
+        password: {  label: "Password", type: "password" }
+      },
+      async authorize(credentials, req) {
+        const res = await fetch("/your/endpoint", {
+          method: 'POST',
+          body: JSON.stringify(credentials),
+          headers: { "Content-Type": "application/json" }
+        })
+        const user = await res.json()
+  
+        // If no error and we have user data, return it
+        if (res.ok && user) {
+          return user
+        }
+        // Return null if user data could not be retrieved
+        return null
+      }
+    })
+    
   ],
+  pages: {
+    signIn: '/auth/signin',
+    signOut: '/auth/signout',
+    error: '/auth/error',
+    verifyRequest: '/auth/verify-request',
+  },
   secret: process.env.NEXTAUTH_SECRET,
 };
 
