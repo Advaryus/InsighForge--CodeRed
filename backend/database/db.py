4053cf844456846bf17d02e778a6815745378f5d
cheatmsr <yttechcyber@gmail.com>
2025-01-09 18:47:40 +0530

    scraper changes

--- backend/database/db.py
+++ backend/database/db.py
@@ -1,12 +1,15 @@
 from pymongo import MongoClient
 from dotenv import load_dotenv
+from pymongo import MongoClient
 import os
 
-load_dotenv(dotenv_path='../../.env.local')
-mongoUrl = os.getenv('MONGODB_URI')
+load_dotenv(dotenv_path='../.env.local')
+mongoUrl = os.getenv('MONGO_URL')
+nextUrl = os.getenv('NEXTAUTH_SECRET')
+print(f"MONGODB_URL: {mongoUrl}")
+print(f"NEXTAUTH_URL: {nextUrl}")
 
 client = MongoClient(mongoUrl)
 
 db = client['example']
-
 metadata = db['metadata']
